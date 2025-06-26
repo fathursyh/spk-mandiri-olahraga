82a35dabb9893f17055cafe40a0139bbcbcd1737
fathursyh <fathur.syah1@gmail.com>
2025-06-26 14:01:18 +0700

    update : 1. add AOS 2. add hero 3. add landing page, and kalkulasi page 4. styling navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  prefetch: {
+    prefetchAll: true, 
+    defaultStrategy: 'hover'
+  },
+  integrations: [icon()]
 });
